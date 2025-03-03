0b81d1f83d652d2708283a3d4eb46a69a5a165e6
DeusVoult <deusvoult@proton.me>
2025-03-03 22:00:05 +0100

    Exercice 3.1 complété

--- src/App.java
+++ src/App.java
@@ -1,8 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        if (args[0].equals("validate-cert")){
-        exo31 exo = new exo31();
-        exo.validatecert(args);
+        if (args[3].isEmpty()) {
+            System.out.println("Missing certificate name");
+            return;
+        }else{
+        switch(args[0]){
+            case "validate-cert":
+                exo31 exo = new exo31();
+                exo.validatecert(args);
+                break;
+            default:
+                System.out.println("Unknown command");
+                return;
+            }
         }
     }
 }
--- src/exo31.java
+++ src/exo31.java
@@ -1,13 +1,16 @@
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.cert.X509Certificate;
+import java.security.PublicKey;
+import java.security.Signature;
 import java.security.cert.CertificateFactory;
+import java.util.Date;
 
 public class exo31 {
     String filename=null;
-    X509Certificate cert=null;
-    //publickey=null;
+    int Anomalie=0;
     public boolean validatecert(String[] args) {
+        //3.1.1
         switch(args[2]){
             case "PEM":
                 filename=args[3]+".crt";
@@ -17,14 +20,98 @@ public class exo31 {
                 break;
             default :
                 System.out.println("Unknown certificate format");
-                return false;
+                Anomalie++;
+                break;
         }
         try (InputStream inStream = new FileInputStream(filename)) {
             CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-            cert = (X509Certificate) certificateFactory.generateCertificate(inStream);
-            //publickey=cert.getPublicKey();
-            System.out.println("CERTIFICATE :\n" + cert);
-            return true;
+            X509Certificate cert = (X509Certificate) certificateFactory.generateCertificate(inStream);
+            if (cert!=null) {
+                System.out.println("CERTIFICATE :\n" + cert);
+                //3.1.2
+                PublicKey publicKey=cert.getPublicKey();
+                try {
+                    cert.verify(publicKey);
+                    System.out.println("OK | Certificat Valide");
+                } catch (Exception e) {
+                    System.out.println("KO | Verification Echouée: " + e.getMessage());
+                    Anomalie++;
+                }
+                //3.1.3
+                System.out.println("\nSujet : " + cert.getSubjectX500Principal());
+                System.out.println("Emetteur : " + cert.getIssuerX500Principal());   
+                //3.1.4
+                boolean[] keyUsage = cert.getKeyUsage();
+                if (keyUsage !=null){
+                    if ((keyUsage[5] && keyUsage[6]) || (keyUsage[5] && keyUsage[6] && keyUsage[7])){
+                        System.out.println("\nKeyUsage Valide : \nKey_CertSign : " + keyUsage[5] + "\nCrl_Sign : " +keyUsage[6] +"\nDigital_Signature : " + keyUsage[7]);
+                    } else{
+                        System.out.println("KeyUsage Invalide");
+                        Anomalie++;
+                    }
+                }
+                //3.1.5
+                Date notBefore = cert.getNotBefore();
+                Date notAfter = cert.getNotAfter();
+                Date Actuel = new Date();
+                if (Actuel.after(notBefore) && Actuel.before(notAfter)){
+                    System.out.println("Certificat Toujours valide jusqu'au : " + notAfter);
+                }else{
+                    System.out.println("Certificat Expiré");
+                    Anomalie++;
+                }
+                //3.1.6
+                byte[] tbsData = cert.getTBSCertificate();
+                String certSignAlg = cert.getSigAlgName();
+                Signature sign = null;
+                switch (certSignAlg) {
+                    case "SHA256withRSA":
+                        sign  = Signature.getInstance("SHA256withRSA");
+                        break;
+                    case "SHA384withRSA":
+                        sign = Signature.getInstance("SHA384withRSA");
+                        break;
+                    case "SHA512withRSA":
+                        sign = Signature.getInstance("SHA512withRSA");
+                        break;
+                    case "SHA256withECDSA":
+                        sign = Signature.getInstance("SHA256withECDSA");
+                        break;
+                    case "SHA384withECDSA":
+                        sign = Signature.getInstance("SHA384withECDSA");
+                        break;
+                    case "SHA512withECDSA":
+                        sign = Signature.getInstance("SHA512withECDSA");
+                        break;
+                    default:
+                        System.out.println("Algorithme de Signature Inconnu");
+                        Anomalie++;
+                        break;
+                }
+                if (sign != null) {
+                sign.initVerify(publicKey);
+                sign.update(tbsData);
+                boolean isSignValid = sign.verify(cert.getSignature());
+                if (isSignValid){
+                    System.out.println("Signature "+ certSignAlg +" Valide");
+                }else{
+                    System.out.println("Signature Invalide");
+                    Anomalie++;
+                }
+                }else{
+                    System.out.println("Aucune signature dans ce certificat");
+                }
+            }else{
+                System.out.println("Certificate est nulle");
+                Anomalie++;
+            }
+            if (Anomalie==0){
+                System.out.println("Ce certificat a passé tous les tests avec succès");
+                return true;
+            }else{
+                System.out.println("Ce Certificat est invalide");
+                return false;
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return false;
